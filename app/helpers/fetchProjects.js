59621512a50e36e747fd172eeec14443a363b4a1
Bamidele-Alao Sodiq <sodiqalao38@gmail.com>
2023-08-10 14:29:23 +0100

    minor chores

--- app/helpers/fetchProjects.js
+++ app/helpers/fetchProjects.js
@@ -1,5 +1,5 @@
 async function fetchProjects() {
-    const res = await fetch("http://127.0.0.1:5173/api/projects/")
+    const res = await fetch("http://127.0.0.1:5173/api/projects")
     const data = await res.json()
     return data
 }
